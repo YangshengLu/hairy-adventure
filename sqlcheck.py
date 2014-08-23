9d43e0709548af0afe670a80f61255d19a96949d
LuYangSheng <lys701@gmail.com>
2014-08-24 03:33:06 +0800

    remove unuse file. build up project structure

--- sqlcheck.py
+++
@@ -1,9 +0,0 @@
-__author__ = 'luyangsheng'
-import mysql.connector
-
-cnt = mysql.connector.connect(user="root", password="root", database="mysql")
-
-cursor = cnt.cursor()
-cursor.execute("select * from user")
-res = cursor.fetchone()
-print(res)
